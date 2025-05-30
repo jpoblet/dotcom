12ce47aefe370b7f2dd6fddfea8fcc571e1f39b3
Jordi Poblet <hello@jordipoblet.com>
2025-05-30 20:15:32 +0200

    tokens + dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,38 @@
 export default function Home() {
   return (
-    <main className="flex flex-col gap-16 items-center w-full">
-      <section>
-        Section 1
+    <main className="flex flex-col items-center w-full">
+      <section className="flex flex-col gap-2 w-full p-8 bg-backgrounud items-center">
+        <div className="text-foreground">
+          Primary Text
+          </div>
+          <div className="text-foreground-secondary">
+          Secondary Text
+          </div>
+          <div className="text-accent">
+          Accent Text
+          </div>
       </section>
-      <section>
-        Section 2
+      <section className="flex flex-col gap-2 w-full p-8 bg-background-secondary items-center">
+        <div className="text-foreground">
+          Primary Text
+          </div>
+          <div className="text-foreground-secondary">
+          Secondary Text
+          </div>
+          <div className="text-accent">
+          Accent Text
+          </div>
       </section>
-      <section>
-        Section 3
+      <section className="flex flex-col gap-2 w-full p-8 bg-accent items-center">
+        <div className="text-foreground">
+          Primary Text
+          </div>
+          <div className="text-foreground/50">
+          Secondary Text
+          </div>
+          <div className="text-foreground-inverse">
+          Accent Text
+          </div>
       </section>
     </main>
   );
